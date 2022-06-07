ae55fff47352a53cb45754797678edc29c1178ea
SergeyKV00 <sergey.kuzichev.v@gmail.com>
2022-06-07 13:54:15 +0300

    Добавьте файлы проекта.

---
+++ BudgetManager/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace BudgetManager.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public DateTime? RevokeOn { get; set; }
+    }
+}
